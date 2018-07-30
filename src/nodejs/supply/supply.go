37bf046fb6124bf526c16053b2b9fac8d3b78eae
Giedrius Valintelis <giedrius.valintels@lt.ibm.com>
2018-07-30 16:05:54 +0300

    Temporary skip dependency moving action.

--- src/nodejs/supply/supply.go
+++ src/nodejs/supply/supply.go
@@ -143,10 +143,10 @@ func Run(s *Supplier) error {
 			return err
 		}
 
-		if err := s.MoveDependencyArtifacts(); err != nil {
-			s.Log.Error("Unable to move dependencies: %s", err.Error())
-			return err
-		}
+//		if err := s.MoveDependencyArtifacts(); err != nil {
+//			s.Log.Error("Unable to move dependencies: %s", err.Error())
+//			return err
+//		}
 
 		s.ListDependencies()
 
